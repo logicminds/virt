fc50f1880cb50574f59889553a652bc11c3c48c9
Corey Osman <corey@logicminds.biz>
2011-12-28 12:12:22 -0800

    added utf8 conversions, to_gb and refined document to parse partial xml text

--- lib/virt/util.rb
+++ lib/virt/util.rb
@@ -1,11 +1,26 @@
 require "rexml/document"
 require 'erb'
+require 'iconv'
 
 module Virt
   module Util
     # return templated xml to be used by libvirt
     def xml
-      ERB.new(template, nil, '-').result(binding)
+      to_utf8 ERB.new(template, nil, '-').result(binding)
+    end
+
+    def to_utf8 myxml
+      # convert string to UTF-8
+      ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+      valid_xml = ic.iconv(myxml + ' ')[0..-2]
+    end
+
+    def to_gb bytes
+      bytes.to_i / 1073741824
+    end
+
+    def to_mb bytes
+      bytes.to_i / 1048576
     end
 
     private
@@ -16,11 +31,20 @@ module Virt
       warn "failed to read template #{template_path}: #{e}"
     end
 
-    # finds a value from xml
-    def document path, attribute=nil
+   # finds a value from xml
+    def document path, attribute=nil, multi=false, inxml=@xml_desc
       return nil if new?
-      xml = REXML::Document.new(@xml_desc)
-      attribute.nil? ? xml.elements[path].text : xml.elements[path].attributes[attribute]
+      xml = REXML::Document.new(inxml)
+      if attribute.nil? and !multi
+          xml.elements[path].text
+      elsif attribute and !multi
+          xml.elements[path].attributes[attribute]
+      elsif multi
+          xml.get_elements(path)
+      else
+          nil
+      end
+
     end
   end
 end
